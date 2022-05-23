5afef84e831c21a4aa37e517adc9fe8749d0957f
UranikSejdiu <51338712+UranikSejdiu@users.noreply.github.com>
2022-05-23 18:06:48 +0200

    trying to get started on produktet everything is setup now we need to do the coding side of things

--- Important Files/pdppn.sql
+++ Important Files/pdppn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 19, 2022 at 12:33 PM
+-- Generation Time: May 23, 2022 at 04:05 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -79,6 +79,19 @@ INSERT INTO `cities` (`id_city`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kategoria`
+--
+
+DROP TABLE IF EXISTS `kategoria`;
+CREATE TABLE IF NOT EXISTS `kategoria` (
+  `kategoriaID` int(11) NOT NULL AUTO_INCREMENT,
+  `kategoria` tinytext NOT NULL,
+  PRIMARY KEY (`kategoriaID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `kompanite`
 --
 
@@ -103,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `kompanite` (
 
 INSERT INTO `kompanite` (`id`, `logo`, `name`, `nrfiskal`, `lokacioni`, `telefoni`, `email`, `password`, `code`, `status`) VALUES
 (2, '../images/kompani/1652531410-627fa0d2304fc.jpg', 'Filan', 111111111, '42.349188,21.17604', '(+383)48/434-177', 'ferizaj0006@gmail.com', '$2y$10$Yvf3/GQmVlvMNrda83SKHe/rwYhYkr8EAV2v2swvUqD4eDDNn6ieS', 0, 'verified'),
-(5, '../images/kompani/1652963525-628638c5308e5.png', 'Uranik Sejdiu', 545454545, '42.560057,20.855082', '(+383)44/879-569', 'u.sejdiu4@gmail.com', '$2y$10$Lee7Nl8gMHAh7BvN6t6NWeUYzKwGq4PfFaQPWumt1OyBAprTIg1Hy', 0, 'verified'),
+(5, '../images/kompani/1652963525-628638c5308e5.png', 'Uranik Sejdiu', 545454598, '42.560057,20.855082', '(+383)44/879-569', 'u.sejdiu4@gmail.com', '$2y$10$Lee7Nl8gMHAh7BvN6t6NWeUYzKwGq4PfFaQPWumt1OyBAprTIg1Hy', 0, 'verified'),
 (10, '../images/kompani/1652791679-6283997f66b8b.png', 'Hewlet Packard', 555555555, '42.697829,23.322001', '(+383)45/555-555', 'u.sejdiu56@gmail.com', '$2y$10$AbP.FHKxvt9xUJMVdVsupu3GO9Wj8KN2VwjWQojtd/7jBsoSy8m36', 0, 'verified');
 
 -- --------------------------------------------------------
@@ -133,6 +146,49 @@ INSERT INTO `kontaktet` (`id`, `subjekti`, `mesazhi`, `moduli`, `createdDate`) V
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `madhesit`
+--
+
+DROP TABLE IF EXISTS `madhesit`;
+CREATE TABLE IF NOT EXISTS `madhesit` (
+  `madhesiaID` int(11) NOT NULL AUTO_INCREMENT,
+  `madhesia` varchar(50) NOT NULL,
+  `kategoriaID` int(11) NOT NULL,
+  PRIMARY KEY (`madhesiaID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nenkategoria`
+--
+
+DROP TABLE IF EXISTS `nenkategoria`;
+CREATE TABLE IF NOT EXISTS `nenkategoria` (
+  `nenKategoriaID` int(11) NOT NULL AUTO_INCREMENT,
+  `nenKategoria` tinytext NOT NULL,
+  `kategoriaID` int(11) NOT NULL,
+  PRIMARY KEY (`nenKategoriaID`),
+  KEY `subCatForeignKey` (`kategoriaID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ngjyrat`
+--
+
+DROP TABLE IF EXISTS `ngjyrat`;
+CREATE TABLE IF NOT EXISTS `ngjyrat` (
+  `ngjyratID` int(11) NOT NULL AUTO_INCREMENT,
+  `ngjyra` varchar(50) NOT NULL,
+  `kategoriaID` int(11) NOT NULL,
+  PRIMARY KEY (`ngjyratID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `perdoruesit`
 --
 
@@ -158,15 +214,61 @@ CREATE TABLE IF NOT EXISTS `perdoruesit` (
 INSERT INTO `perdoruesit` (`id`, `fullName`, `id_city`, `adress`, `phone`, `email`, `password`, `code`, `status`) VALUES
 (2, 'Filan Fisteku', 2, 'Mataj', '(+383)45/333-333', 'ferizaj0004@gmail.com', '$2y$10$e5FDa2mXvJ3IiDmnEAVeIeMrzT7oUMRtEYNgE4ZCAiLHRMBctdldS', 547301, 'notverified');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produktet`
+--
+
+DROP TABLE IF EXISTS `produktet`;
+CREATE TABLE IF NOT EXISTS `produktet` (
+  `produktID` int(11) NOT NULL AUTO_INCREMENT,
+  `produkti` varchar(150) NOT NULL,
+  `imazhi1` varchar(250) NOT NULL,
+  `imazhi2` varchar(250) NOT NULL,
+  `imazhi3` varchar(250) NOT NULL,
+  `imazhi4` varchar(250) NOT NULL,
+  `kategoriaID` int(11) NOT NULL,
+  `nenKategoriaID` int(11) DEFAULT NULL,
+  `pershkrimi` mediumtext NOT NULL,
+  `qmimi` decimal(15,2) NOT NULL,
+  `stoku` int(11) NOT NULL,
+  `madhesiaID` int(11) DEFAULT NULL,
+  `ngjyraID` int(11) DEFAULT NULL,
+  `kompaniaID` int(11) NOT NULL,
+  PRIMARY KEY (`produktID`),
+  KEY `katForeignKey` (`kategoriaID`),
+  KEY `sizeForeignKey` (`madhesiaID`),
+  KEY `colorForeignKey` (`ngjyraID`),
+  KEY `nenKatForeignKey` (`nenKategoriaID`),
+  KEY `kompaniaForeignKey` (`kompaniaID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `nenkategoria`
+--
+ALTER TABLE `nenkategoria`
+  ADD CONSTRAINT `subCatForeignKey` FOREIGN KEY (`kategoriaID`) REFERENCES `kategoria` (`kategoriaID`);
 
 --
 -- Constraints for table `perdoruesit`
 --
 ALTER TABLE `perdoruesit`
   ADD CONSTRAINT `cityForeignKey` FOREIGN KEY (`id_city`) REFERENCES `cities` (`id_city`);
+
+--
+-- Constraints for table `produktet`
+--
+ALTER TABLE `produktet`
+  ADD CONSTRAINT `colorForeignKey` FOREIGN KEY (`ngjyraID`) REFERENCES `ngjyrat` (`ngjyratID`),
+  ADD CONSTRAINT `katForeignKey` FOREIGN KEY (`kategoriaID`) REFERENCES `kategoria` (`kategoriaID`),
+  ADD CONSTRAINT `kompaniaForeignKey` FOREIGN KEY (`kompaniaID`) REFERENCES `kompanite` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nenKatForeignKey` FOREIGN KEY (`nenKategoriaID`) REFERENCES `nenkategoria` (`nenKategoriaID`),
+  ADD CONSTRAINT `sizeForeignKey` FOREIGN KEY (`madhesiaID`) REFERENCES `madhesit` (`madhesiaID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Important Files/readme.txt
+++ Important Files/readme.txt
@@ -6,3 +6,6 @@ https://gist.github.com/danieldogeanu/92e7f0b4d584842994521f08dafc306d
 
 tutorial:
 https://www.youtube.com/watch?v=GzpjBMNthfE
+
+Select options based on what is selected:
+https://stackoverflow.com/questions/18755872/jquery-show-hide-drop-down-options-based-on-another-drop-down-option
