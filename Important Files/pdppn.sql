485fbd2b224c7c276876774ddce86520bfa1938a
UranikSejdiu <51338712+UranikSejdiu@users.noreply.github.com>
2022-05-31 17:38:48 +0200

    db backup

--- Important Files/pdppn.sql
+++ Important Files/pdppn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 31, 2022 at 12:31 PM
+-- Generation Time: May 31, 2022 at 03:38 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -280,8 +280,6 @@ CREATE TABLE IF NOT EXISTS `produktet` (
 INSERT INTO `produktet` (`produktID`, `produkti`, `imazhi1`, `imazhi2`, `imazhi3`, `imazhi4`, `kategoriaID`, `pershkrimi`, `qmimi`, `stoku`, `madhesiaID`, `ngjyraID`, `kompaniaID`) VALUES
 (17, 'Hewlet Packard', '../images/produktet/1653659980-6290d94c992e4.png', '../images/produktet/1653659980-6290d94c99601.png', '../images/produktet/1653659980-6290d94c99611.png', '../images/produktet/1653659980-6290d94c99625.png', 1, 'asdasdasdadsads', '21.00', 1, 1, 3, 5),
 (18, 'PDPPN', '../images/produktet/1653660030-6290d97e20592.png', '../images/produktet/1654000235-62960a6b7f1bf.jpg', '../images/produktet/1654000235-62960a6b7f9b8.png', '../images/produktet/1653660030-6290d97e2088a.png', 1, 'TESTtest TESTtest TESTtest TESTtest TESTtest TESTtest', '122.00', 12, 1, 3, 5),
-(19, 'Company', '../images/produktet/1653660319-6290da9f50ee8.png', '../images/produktet/1653999292-629606bcd4961.png', '../images/produktet/1653999292-629606bcd51de.png', '../images/produktet/1653660319-6290da9f52e33.png', 2, 'done done', '123.32', 56, 4, 4, 2),
-(20, 'Company2nd', '../images/produktet/1653660391-6290dae71e58d.jpg', '../images/produktet/1653660391-6290dae71f14b.jpg', '../images/produktet/1653660391-6290dae71f15c.jpg', '../images/produktet/1653660391-6290dae71f165.jpg', 1, 'asdasdasdasdads', '155.96', 3, 1, 2, 2),
 (21, 'Filan Fisteku', '../images/produktet/1653660434-6290db12c4a63.jpg', '../images/produktet/1653998323-629602f37fe76.jpg', '../images/produktet/1653998323-629602f3805cd.jpg', '../images/produktet/1653998468-62960384a120e.jpg', 1, 'asdasdasdadsadsadsasd', '1.00', 1, 2, 1, 2),
 (22, 'test', '../images/produktet/1653998550-629603d62aaa2.jpg', '../images/produktet/1653998512-629603b00d7ee.jpg', '../images/produktet/1653998442-6296036aac79e.jpg', '../images/produktet/1653998442-6296036aac7b0.jpg', 2, '44545453', '45.00', 45, 4, 4, 2),
 (23, '111', '../images/produktet/1653660744-6290dc48ca7f9.png', '../images/produktet/1653998930-629605529d743.jpg', '../images/produktet/1653998930-629605529dc2d.jpg', '../images/produktet/1653998930-629605529dc3d.jpg', 1, '111', '10.00', 10, 1, 2, 2);
