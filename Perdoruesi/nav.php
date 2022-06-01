aad33471fe3f6773f3795a0af63ed1c688137942
UranikSejdiu <51338712+UranikSejdiu@users.noreply.github.com>
2022-06-01 16:09:57 +0200

    Perdoruesi Modul
    just started working on perdoruesi modul and need to work on it from now on and complete it this week all of it slowly but needs to be all complete and working

---
+++ Perdoruesi/nav.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 col-lg-2 col-sm-3 col-xs-3">
+            <div class="logo">
+                <a href="home.php">
+                    <img src="images/logo/logo.png" alt="logo">
+                </a>
+            </div>
+        </div>
+        <!-- Start MAinmenu Ares -->
+        <div class="col-md-8 col-lg-8 col-sm-6 col-xs-6">
+            <nav class="mainmenu__nav hidden-xs hidden-sm">
+                <ul class="main__menu">
+                    <li><a href="home.php">Kryefaçja</a></li>
+                    <li><a href="produktet.php">Produktet</a></li>
+                    <li><a href="kontakto.php">Kontakti</a></li>
+                </ul>
+
+            </nav>
+            <div class="mobile-menu clearfix visible-xs visible-sm">
+                <nav id="mobile_dropdown">
+                    <ul>
+                        <li><a href="home.php">Kryefaçja</a></li>
+                        <li><a href="produkt.php">Produktet</a></li>
+                        <li><a href="kontakt.php">Kontakti</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <!-- End MAinmenu Ares -->
+        <div class="col-md-2 col-sm-4 col-xs-3">
+            <ul class="menu-extra">
+                <?php echo '<li><a href="profile.php?ID='.$id.'"><span aria-label="Profili" data-cooltipz-dir="bottom"><i class="ti-user"></i></span></a></li>'; ?>
+                <li><a href="logout-user.php"><span aria-label="Çkyçu" data-cooltipz-dir="bottom"><i class="ti-unlink"></i></span></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="mobile-menu-area"></div>
+</div>
