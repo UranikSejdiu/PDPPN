88114eb62164fea1bf794559c3eee7a10bf7e658
UranikSejdiu <51338712+UranikSejdiu@users.noreply.github.com>
2022-07-12 12:06:12 +0200

    nothing

--- Kompani/home.php
+++ Kompani/home.php
@@ -1,6 +1,7 @@
 <?php include_once('checkSession.php'); ?>
 <!doctype html>
 <html class="no-js" lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
